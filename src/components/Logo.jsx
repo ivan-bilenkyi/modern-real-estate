1f22b26fe672971c3ee0c95a469ba2f58187ce62
ivanbilenkyi <ivanbilenky@gmail.com>
2024-06-08 00:39:32 +0300

    fix directory

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export const Logo = () =>  {
-    return (
-        <h1 className='text-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-        </h1>
-    )
-}
