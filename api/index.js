1a63886970b3e23009c753a3f30e416935bc03a4
ivanbilenkyi <ivanbilenky@gmail.com>
2024-06-08 16:18:57 +0300

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -14,10 +18,13 @@ mongoose
         console.log(err);
     });
 
+const __dirname = path.resolve();
+
 const app = express();
 
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -25,6 +32,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
